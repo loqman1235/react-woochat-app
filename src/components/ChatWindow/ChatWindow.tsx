56ff392c3ab9cf376959ba70e0d7c3cbc74b5534
loqman1235 <tonyvito12@gmail.com>
2024-04-18 09:25:44 +0100

    commit

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -2,13 +2,19 @@ import { MdClose, MdMinimize, MdOpenInFull } from "react-icons/md";
 import Avatar from "../shared/Avatar";
 import ChatInput from "../ChatArea/ChatInput";
 import useChatWindow from "@/hooks/useChatWindow";
+import { useState } from "react";
 
 const ChatWindow = () => {
   const { isChatWindowOpen, toggleChatWindow, currentUser } = useChatWindow();
+  const [isMinimized, setIsMinimized] = useState(false);
+
+  const toggleMinimize = () => {
+    setIsMinimized((prev) => !prev);
+  };
 
   return (
     <div
-      className={`absolute bottom-0 right-0 z-40 w-full shadow-2xl  md:w-[400px] ${isChatWindowOpen ? "block" : "hidden"}`}
+      className={`absolute bottom-0 right-0 z-40 w-full cursor-pointer shadow-2xl transition-transform duration-500 ease-[cubic-bezier(0.87,0,0.13,1)] md:w-[380px] ${isChatWindowOpen ? "block" : "hidden"} ${isMinimized ? "translate-y-[calc(100%-40px)]" : "translate-y-0"} `}
     >
       {/* HEADER */}
       <div className="flex items-center justify-between overflow-hidden rounded-tl-md rounded-tr-md border border-border bg-muted px-5 py-1">
@@ -26,11 +32,14 @@ const ChatWindow = () => {
         </div>
 
         {/* CTAS */}
-        <div className="flex h-full items-center gap-5 text-xl">
+        <div className="flex h-full items-center gap-5 text-xl text-text-foreground">
           <button className="transition duration-300 hover:text-text-foreground">
             <MdOpenInFull />
           </button>
-          <button className="mb-2 transition duration-300 hover:text-text-foreground">
+          <button
+            className="mb-2 transition duration-300 hover:text-text-foreground"
+            onClick={toggleMinimize}
+          >
             <MdMinimize />
           </button>
           <button
