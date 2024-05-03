2567b635be6266beaeeff2cb6a8f387e4ac48fb7
loqman1235 <tonyvito12@gmail.com>
2024-05-03 09:16:39 +0100

    added `refresh token` logic

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -130,6 +130,8 @@ export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
     };
 
     if (accessToken) verifyAccessToken();
+
+    // if (!accessToken) signoutUser();
   }, [accessToken]);
 
   return (
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,50 @@
+import { getItemFromLocalStorage, setItemToLocalStorage } from "@/utils";
 import axios from "axios";
 
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
   withCredentials: true,
 });
+
+// Append Authorization header
+api.interceptors.request.use((config) => {
+  const accessToken = getItemFromLocalStorage("accessToken");
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+// Refresh token interceptor
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const res = await api.post("/auth/refresh-token");
+
+        if (res.status === 200) {
+          const { accessToken: newAccessToken, user } = res.data;
+
+          setItemToLocalStorage("accessToken", newAccessToken);
+          setItemToLocalStorage("user", JSON.stringify(user));
+
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+          return api(originalRequest);
+        }
+      } catch (error) {
+        console.log("Error refreshing token: ", error);
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
