52a8a4243f6f99502f601aaabe22aba3f08cbe08
loqman1235 <tonyvito12@gmail.com>
2024-05-09 16:52:02 +0100

    commit

--- src/components/shared/FormField/FormField.tsx
+++ src/components/shared/FormField/FormField.tsx
@@ -3,7 +3,7 @@ import { FieldValues, UseFormRegister } from "react-hook-form";
 import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 
 interface FormFieldProps<TFieldValues extends FieldValues = FieldValues> {
-  type: "text" | "email" | "password" | "date";
+  type: "text" | "email" | "password" | "date" | "number" | "textarea";
   label: string;
   name: keyof TFieldValues;
   id: string;
@@ -30,42 +30,55 @@ const FormField = ({
   const toggleShowPassword = () => setShowPassword((prev) => !prev);
 
   return (
-    <div className="relative space-y-1">
+    <div className={`relative space-y-1 ${className}`}>
       <label htmlFor={id} className="text-sm text-text-foreground">
         {label}{" "}
         {required ? <span className="text-xs text-danger">*</span> : null}
       </label>
-      <div className="relative">
-        <input
-          type={type === "password" && showPassword ? "text" : type}
+      {type !== "textarea" && (
+        <div className="relative">
+          <input
+            type={type === "password" && showPassword ? "text" : type}
+            id={id}
+            placeholder={placeholder}
+            className={`w-full rounded-md border bg-background px-3 py-2 text-text-foreground outline-none placeholder:text-text-muted  ${error ? "border-danger" : "border-border"}`}
+            required={required}
+            autoComplete="off"
+            {...register(name)}
+          />
+          {type === "password" && (
+            <button
+              type="button"
+              className="absolute right-2 top-1/2 -translate-y-1/2 text-text-muted"
+              onClick={(e) => {
+                e.preventDefault();
+                toggleShowPassword();
+              }}
+            >
+              {showPassword ? (
+                <span>
+                  <MdVisibilityOff />
+                </span>
+              ) : (
+                <span>
+                  <MdVisibility />
+                </span>
+              )}
+            </button>
+          )}
+        </div>
+      )}
+      {type === "textarea" && (
+        <textarea
+          className={`w-full rounded-md border bg-background px-3 py-2 text-text-foreground outline-none placeholder:text-text-muted  ${error ? "border-danger" : "border-border"}`}
+          {...register(name)}
           id={id}
           placeholder={placeholder}
-          className={`w-full rounded-md border bg-background px-3 py-2 text-text-foreground outline-none placeholder:text-text-muted ${className} ${error ? "border-danger" : "border-border"}`}
-          required={required}
           autoComplete="off"
-          {...register(name)}
+          required={required}
+          rows={5}
         />
-        {type === "password" && (
-          <button
-            type="button"
-            className="absolute right-2 top-1/2 -translate-y-1/2 text-text-muted"
-            onClick={(e) => {
-              e.preventDefault();
-              toggleShowPassword();
-            }}
-          >
-            {showPassword ? (
-              <span>
-                <MdVisibilityOff />
-              </span>
-            ) : (
-              <span>
-                <MdVisibility />
-              </span>
-            )}
-          </button>
-        )}
-      </div>
+      )}
       {error && <p className="text-xs text-danger">{error}</p>}
     </div>
   );
--- src/components/shared/Modal/AboutForm.tsx
+++ src/components/shared/Modal/AboutForm.tsx
@@ -1,50 +1,110 @@
 import { MdSave } from "react-icons/md";
 import Button from "../Button";
+import { FormField } from "../FormField";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { toast } from "react-toastify";
+import { setItemToLocalStorage } from "@/utils";
+import api from "@/services/api";
 
+// Hooks
+import useProfile from "@/hooks/useProfile";
+import useAuth from "@/hooks/useAuth";
 interface AboutFormProps {
   isOpen: boolean;
 }
 
+const aboutSchema = z.object({
+  age: z.coerce.number().nullable().optional(),
+  mood: z.string().nullable().optional(),
+  about: z.string().nullable().optional(),
+});
+
+type AboutForm = z.infer<typeof aboutSchema>;
+
 const AboutForm = ({ isOpen = false }: AboutFormProps) => {
+  const { user, setUser } = useAuth();
+  // const { data: fetchedUser, isLoading } = useFetch<User>("/users", user?.id);
+  const { setCurrentUser } = useProfile();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AboutForm>({
+    resolver: zodResolver(aboutSchema),
+    defaultValues: user
+      ? { age: user.age, mood: user.mood, about: user.about }
+      : {},
+  });
+
+  const onSubmit: SubmitHandler<AboutForm> = async (data) => {
+    const formData = new FormData();
+
+    if (data.age) {
+      formData.append("age", data.age.toString());
+    }
+
+    if (data.mood) {
+      formData.append("mood", data.mood);
+    }
+
+    if (data.about) {
+      formData.append("about", data.about);
+    }
+
+    try {
+      const response = await api.put(`/users/${user?.id}`, formData);
+
+      if (response.status === 200) {
+        toast.success("Profile updated successfully");
+        setCurrentUser(response.data.user);
+        setUser(response.data.user);
+        setItemToLocalStorage("user", JSON.stringify(response.data.user));
+      }
+    } catch (error) {
+      toast.error("Error updating profile");
+    }
+  };
+
   return (
     <div className={`w-full p-5 ${isOpen ? "block" : "hidden"}`}>
-      <form className="flex flex-col gap-5 text-text-foreground">
+      <form
+        className="flex flex-col gap-5 text-text-foreground"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="flex w-full items-center gap-5">
-          <div className="w-1/2 space-y-1">
-            <label htmlFor="age" className="text-sm text-text-foreground">
-              Age
-            </label>
-            <input
-              type="number"
-              name="age"
-              id="age"
-              className="w-full rounded-md border border-border bg-background p-2 outline-none placeholder:text-text-muted"
-            />
-          </div>
-          <div className="w-1/2 space-y-1">
-            <label htmlFor="gender" className="text-sm text-text-foreground">
-              Gender
-            </label>
-            <select
-              name="gender"
-              id="gender"
-              className="w-full rounded-md border border-border bg-background p-2 outline-none placeholder:text-text-muted"
-            >
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-          </div>
+          <FormField
+            label="Age"
+            name="age"
+            placeholder="Enter your age"
+            type="number"
+            id="age"
+            register={register}
+            className="w-1/2"
+            error={errors.age?.message}
+          />
+          <FormField
+            label="Mood"
+            name="mood"
+            placeholder="Enter your mood"
+            type="text"
+            id="mood"
+            className="w-1/2"
+            register={register}
+            error={errors.mood?.message}
+          />
         </div>
-        <div className="w-full space-y-1">
-          <label htmlFor="about" className="text-sm text-text-foreground">
-            About
-          </label>
-          <textarea
-            name="about"
-            id="about"
-            className="min-h-[120px] w-full resize-none rounded-md border border-border bg-background p-2 outline-none placeholder:text-text-muted"
-          ></textarea>
-        </div>
+        <FormField
+          type="textarea"
+          label="About"
+          name="about"
+          id="about"
+          placeholder="Enter your about"
+          register={register}
+          error={errors.about?.message}
+        />
         <Button type="submit" variant="primary">
           <MdSave /> Save
         </Button>
