cda3a5b390fa3519baf6661ba20953cbf17c623d
loqman1235 <tonyvito12@gmail.com>
2024-05-17 08:30:20 +0100

    commit

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,4 +1,4 @@
-import Footer from "@/components/Footer";
+// import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { ProfileModal } from "@/components/Profile";
 import { ChatWindoProvider } from "@/context/ChatWindowContext";
